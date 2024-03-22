d9cd79526a000f5b80c67c7d886b1f2aa01592ab
Eduardo Camana <111513082+camana98@users.noreply.github.com>
2024-03-22 17:59:22 -0300

    Update App.java mudei textos para ingles

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
   public static void main (String[] args){
-    System.out.println("Olá, pessoal.");
+    //Meu primeiro programa
+    System.out.println("Hello, everyone!");
   }
 }
